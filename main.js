9584331469f7cef984f6cadd0885525a841ebdbc
val_l <contacttovalerie@gmail.com>
2025-02-25 01:58:35 -0500

    Added columns for 5 and 15

--- main.js
+++ main.js
@@ -10,33 +10,42 @@ const numberClicker = get('.number-clicker');
 const evenNumbers = get('.even-numbers');
 const oddNumbers = get('.odd-numbers');
 const by3Numbers = get('.divisible-3-numbers');
+const by5Numbers = get('.divisible-5-numbers');
+const by15Numbers = get('.divisible-15-numbers');
+
 
 let count = 0;
-//let newCount;
 
 const addNumbertoDiv = function(numberDiv, list, newCount) {
     numberDiv.classList.add('number-clicker');
     numberDiv.innerText = `${newCount}`;
     list.appendChild(numberDiv);
+
+    numberDiv.addEventListener('click', function() {
+        numberDiv.remove();
+    });
 }    
 
 numberClicker.addEventListener('click', function() {
     let newCount = ++count;
     numberClicker.innerText = newCount;
 
-    if (newCount % 2 === 0) {
-        const newEvenNumber = newDiv('div');
-        addNumbertoDiv(newEvenNumber, evenNumbers, newCount);
-
-    } else if (newCount % 2 !== 0) {
-        const newOddNumber = newDiv('div');
-        addNumbertoDiv(newOddNumber, oddNumbers, newCount);
-        } 
-
-    if (newCount % 3 === 0) {
-        const newBy3Number = newDiv('div');
+    if (newCount % 15 === 0) {
+        const newBy15Number = newDiv('button');
+        addNumbertoDiv(newBy15Number, by15Numbers, newCount);
+    } else if (newCount % 5 === 0) {
+        const newBy5Number = newDiv('button');
+        addNumbertoDiv(newBy5Number, by5Numbers, newCount);
+    } else if (newCount % 3 === 0) {
+        const newBy3Number = newDiv('button');
         addNumbertoDiv(newBy3Number, by3Numbers, newCount);
-        }
+    } else if (newCount % 2 === 0) {
+        const newEvenNumber = newDiv('button');
+        addNumbertoDiv(newEvenNumber, evenNumbers, newCount);
+    } else if (newCount % 2 !== 0) {
+        const newOddNumber = newDiv('button');
+        addNumbertoDiv(newOddNumber, oddNumbers, newCount);
+    } 
 });
 
 
