8325a6f7de45d61be82a5bfa0a7aa2b65c9200c6
val_l <contacttovalerie@gmail.com>
2025-02-26 02:18:44 -0500

    Condensed JS

--- main.js
+++ main.js
@@ -1,52 +1,46 @@
-const get = function(selector) {
+const get = function (selector) {
     return document.querySelector(selector);
-}
-
-const newDiv = function(div){
-    return document.createElement(div);
-}
-
-const numberClicker = get('.number-clicker');
-const evenNumbers = get('.even-numbers');
-const oddNumbers = get('.odd-numbers');
-const by3Numbers = get('.divisible-3-numbers');
-const by5Numbers = get('.divisible-5-numbers');
-const by15Numbers = get('.divisible-15-numbers');
-
-
-let count = 0;
-
-const addNumbertoDiv = function(numberDiv, list, newCount) {
-    numberDiv.classList.add('number-clicker');
-    numberDiv.innerText = `${newCount}`;
-    list.appendChild(numberDiv);
-
-    numberDiv.addEventListener('click', function() {
-        numberDiv.remove();
-    });
-}    
-
-numberClicker.addEventListener('click', function() {
+  };
+  
+  const newElement = function (element) {
+    return document.createElement(element);
+  };
+  
+  const numberClicker = get(".number-clicker");
+  const evenNumbers = get(".even-numbers");
+  const oddNumbers = get(".odd-numbers");
+  const by3Numbers = get(".divisible-3-numbers");
+  const by5Numbers = get(".divisible-5-numbers");
+  const by15Numbers = get(".divisible-15-numbers");
+  
+  let count = 0;
+  
+  numberClicker.addEventListener("click", function () {
     let newCount = ++count;
     numberClicker.innerText = newCount;
+    const numberButton = newElement("button");
+    numberButton.classList.add("number-clicker");
+    numberButton.innerText = `${newCount}`;
 
     if (newCount % 15 === 0) {
-        const newBy15Number = newDiv('button');
-        addNumbertoDiv(newBy15Number, by15Numbers, newCount);
+      by15Numbers.appendChild(numberButton);
+
     } else if (newCount % 5 === 0) {
-        const newBy5Number = newDiv('button');
-        addNumbertoDiv(newBy5Number, by5Numbers, newCount);
+      by5Numbers.appendChild(numberButton);
+
     } else if (newCount % 3 === 0) {
-        const newBy3Number = newDiv('button');
-        addNumbertoDiv(newBy3Number, by3Numbers, newCount);
+      by3Numbers.appendChild(numberButton);
+
     } else if (newCount % 2 === 0) {
-        const newEvenNumber = newDiv('button');
-        addNumbertoDiv(newEvenNumber, evenNumbers, newCount);
-    } else if (newCount % 2 !== 0) {
-        const newOddNumber = newDiv('button');
-        addNumbertoDiv(newOddNumber, oddNumbers, newCount);
-    } 
-});
+      evenNumbers.appendChild(numberButton);
 
+    } else {
+      oddNumbers.appendChild(numberButton);
+    }
 
+    numberButton.addEventListener("click", function () {
+        numberButton.remove();
+      });
+  });
+  
 
